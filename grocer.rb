d6a90b6b54e9c9e65087344f5d8ac9cac8843575
Michael Diaz <mike.diaz006@gmail.com>
2019-12-16 20:10:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,15 @@
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.each do |name_and_info_of_food|
+    name_of_food = name_and_info_of_food.keys[0]
+    if final_hash.has_key?(name_of_food)
+      final_hash[name_of_food][:count] += 1 
+    else
+      final_hash[name_of_food] = name_and_info_of_food.values[0]
+      final_hash[name_of_food][:count] = 1
+    end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
